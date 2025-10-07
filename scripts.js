8c851b1535f9188cea868d23166c70673fa26a3d
CARLOS <carl.gomezro@gmail.com>
2025-10-07 15:53:16 +0200

    resolved

--- scripts.js
+++ scripts.js
@@ -15,7 +15,7 @@ const images = document.querySelectorAll('img');
 
 for (let image of images) {
   image.addEventListener('click',() => {
-    image.src = "./assets/magic-1.gif"
+    image.src =  gifs[getRandom(gifs.length)]
   });
 }
 
@@ -25,8 +25,8 @@ const parrafos = document.querySelectorAll('p');
 
 for (let parrafo of parrafos) {
   parrafo.addEventListener('click',() => {
-    parrafo.style.color = coloors[getRandom(coloors.length)]
-    parrafo.style.backgroundColor = coloors[getRandom(coloors.length)]
+    parrafo.style.color = colors2[getRandom(colors2.length)]
+    parrafo.style.backgroundColor = colors2[getRandom(colors2.length)]
   });
 }
 
@@ -36,7 +36,7 @@ const secciones = document.querySelectorAll('.post-content');
 
 for (let seccion of secciones) {
   seccion.addEventListener('click',() => {
-    seccion.style.backgroundColor = "#d53f21ff"
+    seccion.style.backgroundColor = colors2[getRandom(colors2.length)]
   });
 }
 
@@ -63,7 +63,7 @@ for (let parrafo of parrafos) {
   const originalBackground = parrafo.style.backgroundColor;
 
   parrafo.addEventListener('mouseover', () => {
-    parrafo.style.color = "#1e929dff";
+    parrafo.style.color = colors2[getRandom(colors2.length)];
     parrafo.style.backgroundColor = "#c5e836ff";
   });
 
@@ -78,7 +78,7 @@ for (let parrafo of parrafos) {
 
 for (let seccion of secciones) {
   seccion.addEventListener('mouseover', () => {
-    seccion.style.backgroundColor = "#a11313ff";
+    seccion.style.backgroundColor = colors2[getRandom(colors2.length)];
   });
 
   seccion.addEventListener('mouseout', () => {
@@ -89,8 +89,8 @@ for (let seccion of secciones) {
 //BONUS EJERCICIO 4
 const colors = ['red', 'blue', 'green']
 
-function getRandom(arraylongi) {
-  const randomIndex = Math.floor(Math.random() * arraylongi)
+function getRandom(arraylongitud) {
+  const randomIndex = Math.floor(Math.random() * arraylongitud)
    // //Math.random genera un numero aleatorio entre 0 y 0,9999.. y lo multiplicamos por la longitud del array. Math floor, redondead a un numero entero por debajo.
    // Así calculamos el índice aleatorio
 return randomIndex
@@ -100,5 +100,18 @@ console.log(colors[getRandom(colors.length)])
 
 // //BONUS EJERCICIO 5
 
-const coloors = ['#51A3A3', '#75485E','#CB904D','#DFCC74','#C3E991']
-coloors[getRandom(coloors.length)]
+const colors2 = ['#51A3A3', '#75485E','#CB904D','#DFCC74','#C3E991']
+colors2[getRandom(colors2.length)]
+
+// //BONUS EJERCICIO 6
+
+const gifs = [ "./assets/abracadabra.gif", 
+    "./assets/magic-1.gif",
+    "./assets/magic-2.gif", 
+    "./assets/magic-3.gif", 
+    "./assets/magic-4.gif", 
+    "./assets/magic-5.gif", 
+    "./assets/magic-6.gif"
+  ]
+
+ gifs[getRandom(gifs.length)]
